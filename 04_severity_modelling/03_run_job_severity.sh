bba87b4ed8be05d056607e50de8d20fad9f4c166
Shoumi Sarkar <shoumisarkar@gmail.com>
2024-10-24 11:28:08 -0400

    Add files via upload

---
+++ 04_severity_modelling/03_run_job_severity.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sleepsecs=$[ ( $RANDOM % 100 ) + 100 ]s
+sleep $sleepsecs
+
+cd /blue/somnath.datta/shoumisarkar/Fluorosis/Codes/04_severity_modelling
+
+pwd; hostname; date
+
+echo "The value of age is $age, corstr is $corstr"  # Debugging line to check the value of age
+
+echo " This script runs at: \n"
+date
+echo "\n\n"
+
+module load R/4
+
+Rscript fit_severity_whole_data_based.R $age $corstr
