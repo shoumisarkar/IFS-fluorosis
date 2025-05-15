7a2926b3486ce92aa885fe4213fd320c3f251433
Shoumi Sarkar <shoumisarkar@gmail.com>
2025-05-15 11:34:00 -0400

    Add files via upload

---
+++ 06_simulation/N=30/03_run_job_presence_MC.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sleepsecs=$[ ( $RANDOM % 100 ) + 100 ]s
+sleep $sleepsecs
+
+cd /blue/somnath.datta/shoumisarkar/Fluorosis/Codes/06_simulation/N=30
+
+pwd; hostname; date
+
+echo "mc_seed=$mc_seed, age=$age"  # Debugging line to check the value of age
+
+echo " This script runs at: \n"
+date
+echo "\n\n"
+
+module load R/4
+
+Rscript 01a_sim_dat_MC_N_30_presence.R $mc_seed $age
