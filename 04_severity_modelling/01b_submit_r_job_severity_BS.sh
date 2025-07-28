26987e8049c5194f2bdabf1b81b5a7ba133b2276
Shoumi Sarkar <shoumisarkar@gmail.com>
2025-07-28 16:08:09 -0400

    Update and rename 01_submit_r_job_severity_BS.sh to 01b_submit_r_job_severity_BS.sh

---
+++ 04_severity_modelling/01b_submit_r_job_severity_BS.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ages=(9 13 17 23) 
+corstrs=("ar1" "independence" "exchangeable" "jackknifed")
+
+for b in {1..500} 
+do
+	for age in "${ages[@]}"
+	do
+		for corstr in "${corstrs[@]}"
+		do
+		    echo "Submitting job for age=$age and corstr=$corstr, b=$b"
+		    sbatch --export=ALL,b=$b,age=$age,corstr=$corstr 02_fit_i_R_severity_BS.slurm
+		done
+	done
+done
