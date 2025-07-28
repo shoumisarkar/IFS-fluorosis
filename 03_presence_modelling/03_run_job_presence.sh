ab5364c3591763fbed300d6440bd8bfd6e11fe1d
Shoumi Sarkar <shoumisarkar@gmail.com>
2025-07-28 15:07:14 -0400

    Update 03_run_job_presence.sh

--- 03_presence_modelling/03_run_job_presence.sh
+++ 03_presence_modelling/03_run_job_presence.sh
@@ -3,7 +3,7 @@
 sleepsecs=$[ ( $RANDOM % 100 ) + 100 ]s
 sleep $sleepsecs
 
-cd /blue/somnath.datta/shoumisarkar/Fluorosis/Codes/03_presence_modelling
+cd /path/to/Fluorosis/Codes/03_presence_modelling
 
 pwd; hostname; date
 
