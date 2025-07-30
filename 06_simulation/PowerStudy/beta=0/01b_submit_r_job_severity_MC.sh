2fb84761a921ab6e0eda45d3db00906d055ca999
Shoumi Sarkar <shoumisarkar@gmail.com>
2025-07-30 13:34:03 -0400

    Update and rename 01_submit_r_job_severity_MC.sh to 01b_submit_r_job_severity_MC.sh

--- 06_simulation/PowerStudy/beta=0/01b_submit_r_job_severity_MC.sh
+++ 06_simulation/PowerStudy/beta=0/01b_submit_r_job_severity_MC.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-ages=(23) #(9 13 17 23) #(9 13 17 23) #(9 13 17 23)
+ages=(9 13 17 23)
 
-#missing_b=(12 66 77 78 81 82 85 86)
-
-for mc_seed in {269..500} #for b in "${missing_b[@]}"
+for mc_seed in {1..500} 
 do
 	for age in "${ages[@]}"
 	do
