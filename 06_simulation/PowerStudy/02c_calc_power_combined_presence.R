34ff61d5579e81ede345786f6bf8b3ddd6f09e1a
Shoumi Sarkar <shoumisarkar@gmail.com>
2025-07-30 13:17:30 -0400

    Update 02c_calc_power_combined_presence.R

--- 06_simulation/PowerStudy/02c_calc_power_combined_presence.R
+++ 06_simulation/PowerStudy/02c_calc_power_combined_presence.R
@@ -2,13 +2,11 @@
 empirical_power = function(target_alpha = 0.05, M=1000, beta=-2)
 {
   
-  setwd(paste0("W:/somnath.datta/shoumisarkar/Fluorosis/Results/06_simulation/PowerStudy/N30 - beta=",
+  setwd(paste0("path/to/Fluorosis/Results/06_simulation/PowerStudy/N30 - beta=",
                beta, "/combined/independence,independence/age23"))
   
   std_estimates = c()
   signif_vec = c()
-  
-  # count = 0
   
   for(mc_seed in 1:M) 
   {
@@ -42,11 +40,7 @@ empirical_power = function(target_alpha = 0.05, M=1000, beta=-2)
     
     signif_vec = c(signif_vec, is_signif)
     std_estimates = c(std_estimates, std_estimate)
-    
-    # if(count == 100)
-    # {
-    #   break
-    # }
+  
   }
   
   power = length(which(signif_vec))/length((signif_vec))
@@ -78,62 +72,3 @@ plot(x = effect_size[order(effect_size)], y = power[order(effect_size)],
 data.frame(beta = betas,
            effect_size = effect_size,
            power = power)
-
-
-# setwd("W:/somnath.datta/shoumisarkar/Fluorosis/Results/06_simulation/PowerStudy/N30 - zero beta - level/combined/independence,independence/age23")
-# 
-# M = 300
-# 
-# #target_alpha = 0.025
-# 
-# empirical_power = function(target_alpha = 0.05)
-# {
-#   
-#   print(target_alpha) 
-#   
-#   signif_vec = c()
-#   
-#   for(mc_seed in 1:M)
-#   {
-#     filename = paste0("bs_pres_Avghomeppm_MC_", mc_seed,".RData")
-#     
-#     alpha = target_alpha
-#     
-#     if(!file.exists(filename))
-#     {
-#       next
-#     }
-#     
-#     obj = load(filename)
-#     assign("bs_Avghomeppm", get(obj))
-#     
-#     #bs_Avghomeppm = bs_Avghomeppm[1:1000]
-#     
-#     # lci = quantile(bs_Avghomeppm, probs = 0.025, na.rm = T)
-#     # uci = quantile(bs_Avghomeppm, probs = 0.975, na.rm = T)
-#     
-#     
-#     lci = quantile(bs_Avghomeppm, probs = alpha/2, na.rm = T)
-#     uci = quantile(bs_Avghomeppm, probs = (1 - alpha/2), na.rm = T)
-#     
-#     is_signif = T
-#     
-#     if(lci<=0 && 0<=uci)
-#     {
-#       is_signif = F
-#     }
-#     
-#     signif_vec = c(signif_vec, is_signif)
-#     
-#     #print(mc_seed)
-#   }
-#   
-#   print(length(which(signif_vec)))
-#   print(length((signif_vec)))
-#   print(length(which(signif_vec))/length((signif_vec)))
-#   
-#   return(length(which(signif_vec))/length((signif_vec)))
-# }
-# 
-# empirical_power(target_alpha = 0.05)
-# 
